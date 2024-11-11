0c88d5c5a873f7fe885990770c1598ec26052cc1
natanyagaddin <Natanya.gaddin@post.runi.ac.il>
2024-11-11 23:37:57 +0200

    final

--- FVCalc.java
+++ FVCalc.java
@@ -2,5 +2,13 @@
 public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
+int initial = Integer.parseInt(args[0]);
+double interest = (Double.parseDouble(args [1]))/100.0;
+int n = Integer.parseInt(args[2]);
+
+double futurevalue = initial * Math.pow(1+interest,n);
+
+System.out.println("After " + n + " years, $" + initial + " saved at "+ (interest*100)+ "% will yield $"+(int)futurevalue);
+
 	}
 }
